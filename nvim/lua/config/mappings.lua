ca5a9a4a0b7da2c89516a064c7a5dea91b61ef2d
Linheart <leshamihaylenko@bk.ru>
2024-08-27 22:19:50 +0700

    Release

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,8 +1,15 @@
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>n', ':Neotree focus<CR>', { noremap = true, silent = true })
+-- Neotree
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>n", ":Neotree focus<CR>")
 
+-- Window Navigaion
+vim.keymap.set("n", "<leader>h", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { noremap = true, silent = true })
 
+-- Resize Window
+vim.keymap.set("n", "<C-Left>", "<C-w>>")
+vim.keymap.set("n", "<C-Right>", "<C-w><")
+vim.keymap.set("n", "<C-Up>", "<C-w>+")
+vim.keymap.set("n", "<C-Down>", "<C-w>-")
